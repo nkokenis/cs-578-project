83a11305d4d300d81b7fc3e751a6c11df50c9ff1
Garrett <garrettohara2018@gmail.com>
2021-09-01 18:29:11 -0700

    Run bash script from java file

--- TheftPrevention.java
+++ TheftPrevention.java
@@ -1,3 +1,6 @@
+import java.io.*;
+
+
 class TheftPrevention{
 
   public static String getOperatingSysetem() {
@@ -6,6 +9,44 @@ class TheftPrevention{
     return os;
   }
   public static void main(String[] args){
-    System.out.println(getOperatingSysetem());
+    String system = getOperatingSysetem();
+    System.out.println(system);
+    if(system.equals("Mac OS X")){
+      try{
+        
+        executeScript();
+
+      } catch (IOException e) {
+        System.out.println("IO Exception!!");
+        e.printStackTrace();;
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }catch (Exception e){
+        e.printStackTrace();
+      }
+      
+    } else {
+      System.out.println("No");
+    }
+  }
+
+
+  public static void executeScript() throws IOException, InterruptedException {
+    Process p = Runtime.getRuntime().exec("sh /Users/garrettohara/Desktop/sdsu/WirelessNetworks/cs-578-project/SystemDetection.sh");
+    p.waitFor();
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+    BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+
+    String line = "";
+    while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+    }
+
+    line = "";
+    while ((line = errorReader.readLine()) != null) {
+        System.out.println(line);
+    }
   }
 }
