a5ab16ee476aaa4eb5c269f57206934375d7924d
buffman23 <ryancoughlin9@yahoo.com>
2021-09-17 21:05:58 -0700

    Chaged Power detection and renamed bluetooth folder
    -Abstracted power detection to AC_Adapter class
    
    -renamed bluetooth folder to mybluetooth. Was causing issues as pybluez module was also called bluetooth.
    
    added .idea/ and venv/ to .gitignore (folders generated by pycharm)

--- TheftPrevention/power_detection.py
+++ TheftPrevention/power_detection.py
@@ -1,60 +1,106 @@
-from SMS import sms
-from Alarm import play_alarm
-from Camera import open_camera
 import sys
+import threading
+
 import psutil
 import time
-import platform
 
 
-"""
--> Function: Detect Power
-Detect's whether the computer is plugged into AC power
-Calls TheftPrevention functions
--> Parameters:
-N/A
--> Returns:
-N/A
-"""
-def detect_power():
-    battery = psutil.sensors_battery()
-    if(battery is None):
-        print("This device does not have a battery. Exiting...")
+class AC_Adapter:
+    """
+    desc:
+        used to bind functions to adapter plugged and unplugged events.
+
+    Ex:
+        See below class
+    """
+    plugged_listeners = []
+    unplugged_listeners = []
+    thread = None
+    stop = False
+
+    def __init__(self):
+        pass
+
+    def addUnpluggedListener(self, func):
+        self.unplugged_listeners.append(func)
+
+    def removeUnpluggedListener(self, func):
+        self.unplugged_listeners.remove(func)
+
+    def addPluggedListener(self, func):
+        self.plugged_listeners.append(func)
+
+    def removePluggedListener(self, func):
+        self.plugged_listeners.remove(func)
+
+    def listen(self):
+        """
+            desc: Begins AC adapter plug detection
+            return: true if device has a battery, false otherwise
+        """
+        battery = psutil.sensors_battery()
+        if battery is None:
+            # This device does not have a battery.
+            return False
+
+        self.stop = False
+        self.thread = threading.Thread(target=self.__listen, args=())
+        self.thread.start()
+
+        return True
+
+    def stop_listening(self):
+        """
+        desc: Ends AC adapter plug detection
+        """
+        self.stop = True
+
+    def __listen(self):
+        """
+        desc: Private helper function for listen.
+            The function that runs in separate thread.
+        """
+        while True:
+            while psutil.sensors_battery().power_plugged:
+                # Running on AC Power
+                if self.stop:
+                    return
+                time.sleep(1)
+
+            for func in self.unplugged_listeners:
+                func()
+
+            while not psutil.sensors_battery().power_plugged:
+                # Running on Battery Power
+                if self.stop:
+                    return
+                time.sleep(1)
+
+            for func in self.plugged_listeners:
+                func()
+
+######################
+#### EXAMPLE CODE ####
+######################
+
+
+def take_photo():
+    print("Took photo")
+
+
+def send_sms():
+    print("sent sms")
+
+
+if __name__ == '__main__':
+    adapter = AC_Adapter()
+    adapter.addUnpluggedListener(take_photo)
+    adapter.addUnpluggedListener(send_sms)
+
+    success = adapter.listen()
+
+    if not success:
+        print("Device does not have battery. Exiting...")
         sys.exit(1)
-    
-    while(True):
 
-        while(psutil.sensors_battery().power_plugged):
-            print("Running on AC Power")
-            time.sleep(1)
-    
-        sms.send_sms("REPLACE WITH USER PHONE NUMBER")
-        open_camera()
-
-        while(not psutil.sensors_battery().power_plugged):
-            print("Running on Battery Power")
-            play_alarm_mac(detect_system())
-            time.sleep(1)
-
-
-"""
--> Function: detect_system
-Detects what operating system the program is running on 
--> Parameters:
-N/A
--> Returns:
-os: String
-    Operating System name
-"""
-def detect_system():
-    os = platform.system().lower()
-    if os == 'darwin':
-        os = 'mac'
-    return os
-    
-
-# # # Press the green button in the GUI to run the script.
-# if __name__ == '__main__':
-#     detect_power()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    input()
