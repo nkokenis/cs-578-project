b72ea2f4ba5a6005e409cdeba4727a1b876143f7
buffman23 <ryancoughlin9@yahoo.com>
2021-10-06 16:57:22 -0700

    Update btserver.py
    BTServer can now handle client disconnect and wait for a reconnect.
    
    - added useful methods

--- TheftPrevention/mybluetooth/btserver.py
+++ TheftPrevention/mybluetooth/btserver.py
@@ -5,6 +5,7 @@ import bluetooth # pip pybluez22
 
 class BTServer:
     def __init__(self, uuid='e4399be5-b392-4490-a842-cc5abce72cb9'):
+        self.lock = threading.Condition()
         self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         self.server_sock.bind(("", bluetooth.PORT_ANY))
         self.server_sock.listen(1)
@@ -17,68 +18,126 @@ class BTServer:
                                     # protocols=[bluetooth.OBEX_UUID]
                                     )
 
-        self.client_sock, self.client_info = self.server_sock.accept()
-        self.connected = True
-
+        self.online = False
+        self.client_sock = None
+        self.client_info = None
+        self.async_thread = None
         self.data_received_listeners = []
+        self.connect_listeners = []
+        self.disconnect_listeners = []
 
-        self.recv_thread = threading.Thread(target=self.__recv_data, args=())
-        self.recv_thread.start()
+    def start(self):
+        self.online = True
+        self.async_thread = threading.Thread(target=self.__accpet_connections, args=())
+        self.async_thread.start()
+
+    def wait_for_connection(self):
+        if self.client_sock is None:
+            self.lock.acquire()
+            # thread stops here until notified
+            self.lock.wait()
+            self.lock.release()
 
     def send_data(self, data):
         self.client_sock.send(data)
+        
+    def __accpet_connections(self):
+        while self.online:
+            self.client_sock, self.client_info = self.server_sock.accept()
+            # client connected
+            for func in self.connect_listeners:
+                func()
+
+            # notify threads waiting for connection
+            self.lock.acquire()
+            try:
+                self.lock.notify()
+            finally:
+                self.lock.release()
+
+            # start receiving data
+            self.__recv_data()
+
+            # client disconnected
+            for func in self.disconnect_listeners:
+                func()
 
     def __recv_data(self):
         try:
-            while self.connected:
+            while self.online:
                 data = self.client_sock.recv(1024)
-                if data == b'':
-                    self.connected = False
                 for func in self.data_received_listeners:
                     func(data)
+                if data == b'':
+                    break
 
-            self.client_sock.close()
+            self.disconnect_client()
         except OSError:
             pass
 
-    def addDataRecvListener(self, func):
+    def add_data_recv_listener(self, func):
         self.data_received_listeners.append(func)
 
-    def removeDataRecvListener(self, func):
+    def remove_data_recv_listener(self, func):
         self.data_received_listeners.remove(func)
 
-    def disconnect(self):
-        self.connected = False
+    def add_connect_listener(self, func):
+        self.connect_listeners.append(func)
+
+    def remove_connect_listener(self, func):
+        self.connect_listeners.remove(func)
+
+    def add_disconnect_listener(self, func):
+        self.disconnect_listeners.append(func)
+
+    def remove_disconnect_listener(self, func):
+        self.disconnect_listeners.remove(func)
+
+    def shutdown(self):
+        self.online = False
         try:
-            self.client_sock.close()
+            self.disconnect_client()
             self.server_sock.close()
         except OSError:
             pass
-        
-    def isConnected(self):
-        return self.connected
+
+    def disconnect_client(self):
+        try:
+            self.client_sock.close()
+            self.client_sock = None
+        except OSError:
+            pass
+
+    def is_connected(self):
+        return self.client_sock is not None
+
+    def is_online(self):
+        return self.online
 
 
 ######################
 #### EXAMPLE CODE ####
 ######################
 
-def data_received(data):
-    print(data)
-
-
 if __name__ == '__main__':
-    print("BT server waiting for client")
+
     server = BTServer()
-    print("BT server established connection")
-    server.addDataRecvListener(data_received)
+    server.add_data_recv_listener(lambda data: print(data))
+    server.add_connect_listener(lambda: print("Client connected."))
+    server.add_disconnect_listener(lambda: print("Client disconnected."))
+    server.start()
+    print("BT server waiting for client")
+    #server.wait_for_connection()
 
     while True:
         data = input()
         if data == "exit()":
             break
         elif data == "close()":
-            server.disconnect()
+            server.shutdown()
             break
 
-        server.send_data(data)
+        if server.is_connected():
+            server.send_data(data)
+        else:
+            print("No client connected.")
