f6da353b326d71e899c322989bb6f7140ed4e845
Garrett <garrettohara2018@gmail.com>
2021-10-20 22:37:47 -0700

    Changes to alarm

--- TheftPrevention/Alarm.py
+++ TheftPrevention/Alarm.py
@@ -10,13 +10,15 @@ COMMANDS = {
 def play_alarm(operating_system):
     try:
         print("playing alarm")
+
         #The below line sets the computer's volume to max, use with caution
+        # print("System is at max volume")
         # os.system('osascript -e "set Volume 10"')
 
         """
         ADDING LOOP HERE DOES NOT ALLOW FOR CTRL+C commands to stop program
         """
-        print("System is at max volume")
+        
         os.system(COMMANDS[operating_system])
         print("Playing Alarm...")
     except KeyboardInterrupt:
