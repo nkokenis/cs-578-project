364949a38772d23a48f282fd306b5475d41fabdc
Garrett <garrettohara2018@gmail.com>
2021-10-21 12:23:49 -0700

    Working on installation file for program to run smoothly

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo ""
+echo "Thank you for downloading our TheftPrevention Software!!!"
+echo ""
+echo "Let's get things set up for ya"
+echo ""
+python -m ensurepip --upgrade
+echo ""
+sudo pip install -r requirements.txt
+
